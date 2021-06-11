a567572fc1ccc06c6d7b1b5917cfcd1d4def15a0
gilberd30 <gilberd30@gmail.com>
2021-06-11 22:39:19 +0700

    menambahkan route dan method (tugas 1 selesai)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mysql = require('mysql')
+const mysql = require('mysql');
+const { response } = require('express');
 
 const app = express()
 
@@ -14,7 +15,47 @@ const connection = mysql.createConnection({
     database: "database_todolist"
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("DataBase Terhubung");
+});
+
+
+app.get('/', (req, res) => {
+    res.send(`
+    <html>
+        <body>
+            <form action="/todo" method="post">
+                <input name="deskripsi" />
+                <button>Add</button>
+            </form>
+        </body>
+    </html>`)
+})
+
+app.post('/todo', (req, res) => {
+    let deskripsi = req.body.deskripsi
+    connection.query('INSERT INTO tbl_deskripsi (deskripsi) VALUES(?)', [deskripsi], function (error, rows, fileds) {
+        if (error) {
+            console.log(error);
+        } else {
+             console.log("berhasil menambahkan data")
+        }
+        res.end()
+    });
+})
+
+app.get('/todo', (req, res) => {
+    connection.query('SELECT * FROM tbl_deskripsi', function (error, rows, fileds) {
+        if (error) {
+            console.log(error);
+        } else {
+             res.json(rows)
+        }
+        res.end()
+    });
+})
 
 app.listen(3031, function () {
-    console.log("aman lur")
+    console.log("Server Berjalan...")
 })
