7a3201792ecd2a3717da261ffcd89b3363872176
gilberd30 <gilberd30@gmail.com>
2021-06-11 19:54:10 +0700

    membuat koneksi ke database

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "database_todolist"
+});
 
 
 app.listen(3031, function () {
