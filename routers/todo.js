4f30356a89b5e899d54412341bbd844990ad70b1
gilberd30 <gilberd30@gmail.com>
2021-06-13 23:21:20 +0700

    membuat folder database dan routers agar coding lebih rapi

---
+++ routers/todo.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mysql = require('../database/connection')
+const app = express();
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send(`
+    <html>
+        <body>
+            <form action="/todo" method="post">
+                <input name="deskripsi" />
+                <button>Add</button>
+            </form>
+        </body>
+    </html>`)
+})
+
+router.get('/todo', (req, res) => {
+    mysql.query('SELECT * FROM tbl_deskripsi', (err, results, fields) => {
+        if (err) throw err
+        res.json(results)
+    })
+})
+
+router.post('/todo', (req, res) => {
+    const sql = 'INSERT INTO tbl_deskripsi (deskripsi) VALUES(?)'
+    const values = [req.body.deskripsi]
+    mysql.query(sql, values, (err, results, fields) => {
+        if (err) throw err
+        res.json(results)
+    })
+})
+
+router.delete('/todo/:id', (req, res) => {
+    const sql = 'DELETE FROM tbl_deskripsi WHERE id = ?'
+    const values = [req.params.id]
+    mysql.query(sql, values, (err, results, fields) => {
+        if (err) throw err
+        res.json(results)
+    })
+})
+
+module.exports = router
